0854db609ac97a050fa00d53e9668648b8049557
uchexm <109084069+uchexm@users.noreply.github.com>
2024-01-27 20:24:52 +0200

    Update logout logic

--- src/pages/homepage.js
+++ src/pages/homepage.js
@@ -1,12 +1,32 @@
+
+import React, { useContext } from 'react';
+import { Link } from 'react-router-dom';
+import { AuthContext } from "../context/authContext";
+
 function Homepage() {
+  const { user, logout } = useContext(AuthContext);
+
   return (
     <div className="container mt-5">
-      <h3>Welcome to Full Stack Test Movie Data</h3>
-      <p>Watched any movie lately? let us know and browse our awesome catalogue of movies once you log in!</p>
-      <button type="submit" className="btn btn-primary">Login</button>
-      <button type="submit" className="btn btn-primary">Register</button>
+      <>
+        <h3>Welcome to Full Stack Test Movie Data</h3>
+        <p>Watched any movie lately? Let us know and browse our awesome catalogue of movies once you log in!</p>
+        {
+          user ?
+            <>
+              <div> Here is a list of awesom movies added by users. </div>
+            </>
+
+            :
+            <>
+              <Link to="/login" className="btn btn-primary me-2">Login</Link>
+              <Link to="/register" className="btn btn-primary">Register</Link>
+            </>
+        }
+
+      </>
     </div>
-  )
+  );
 }
 
 export default Homepage;
