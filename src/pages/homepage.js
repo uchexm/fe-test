5c2a74d5784b4bdcbbe78758b6fd3429e43ff318
uchexm <109084069+uchexm@users.noreply.github.com>
2024-01-28 10:36:17 +0200

    Refactor Homepage

--- src/pages/homepage.js
+++ src/pages/homepage.js
@@ -1,30 +1,27 @@
-
 import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
 import { AuthContext } from "../context/authContext";
+import Movies from '../components/movies';
 
 function Homepage() {
   const { user, logout } = useContext(AuthContext);
 
   return (
     <div className="container mt-5">
-      <>
-        <h3>Welcome to Full Stack Test Movie Data</h3>
-        <p>Watched any movie lately? Let us know and browse our awesome catalogue of movies once you log in!</p>
-        {
-          user ?
-            <>
-              <div> Here is a list of awesom movies added by users. </div>
-            </>
-
-            :
-            <>
-              <Link to="/login" className="btn btn-primary me-2">Login</Link>
-              <Link to="/register" className="btn btn-primary">Register</Link>
-            </>
-        }
-
-      </>
+      {user ? (
+        <>
+          <h3>OUR LOVED MOVIE DATA</h3>
+          <div> Here is a list of the awesome movies added by our users. </div>
+          <Movies />
+        </>
+      ) : (
+        <>
+          <h3>Welcome to Full Stack Test Movie Data</h3>
+          <p>Watched any movie lately? Let us know and browse our awesome catalogue of movies once you log in!</p>
+          <Link to="/login" className="btn btn-dark me-2">Login</Link>
+          <Link to="/register" className="btn btn-dark">Register</Link>
+        </>
+      )}
     </div>
   );
 }
